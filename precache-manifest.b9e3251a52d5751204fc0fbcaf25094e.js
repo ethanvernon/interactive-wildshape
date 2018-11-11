ca835f3e6f84a315e47ee59e25b6e8ca79a5824b
ethan <ethan.vernon@colorado.edu>
2018-11-11 09:23:14 +0800

    Updates

---
+++ precache-manifest.b9e3251a52d5751204fc0fbcaf25094e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/interactive-wildshape/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e82034db97b47ea9f5bd",
+    "url": "/interactive-wildshape/static/js/runtime~main.e82034db.js"
+  },
+  {
+    "revision": "dc77fc81bd480be70e53",
+    "url": "/interactive-wildshape/static/js/main.dc77fc81.chunk.js"
+  },
+  {
+    "revision": "4b10764f3d12e8205cf6",
+    "url": "/interactive-wildshape/static/js/1.4b10764f.chunk.js"
+  },
+  {
+    "revision": "dc77fc81bd480be70e53",
+    "url": "/interactive-wildshape/static/css/main.6b772304.chunk.css"
+  },
+  {
+    "revision": "6dd993601c7b88e7ae6f314a881225ca",
+    "url": "/interactive-wildshape/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/interactive-wildshape/precache-manifest.b9e3251a52d5751204fc0fbcaf25094e.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/interactive-wildshape/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
